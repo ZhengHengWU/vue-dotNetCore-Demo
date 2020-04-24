f04914fa86a78b448dadb2a094783dd303ba5007
吴正迪 <2274367387@qq.com>
2020-04-24 16:53:00 +0800

    增加保存异常实体和ErrorService

---
+++ dotNetCoreApi/Api.Entity/ErrorEntity.cs
@@ -0,0 +1,82 @@
+﻿using Dapper;
+using System;
+
+
+namespace Api.Entity.Logs
+{
+    /// <summary>
+    /// 错误信息
+    /// </summary>
+    [Table("app_error")]
+    public class ErrorEntity
+    {
+        /// <summary>
+        /// 错误Id
+        /// </summary>
+        [Key]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// 异常信息
+        /// </summary>
+        public string Exception { get; set; }
+
+        /// <summary>
+        /// 当前产品版本
+        /// </summary>
+        public string Version { get; set; } = "";
+
+        /// <summary>
+        /// 操作系统信息
+        /// </summary>
+        public string OsInfo { get; set; } = "";
+
+        /// <summary>
+        /// 错误来源
+        /// </summary>
+        public ErrorFrom ErrorFrom { get; set; } = ErrorFrom.Site;
+
+        /// <summary>
+        ///错误发生时正在处理的数据
+        /// </summary>
+        public string DealData { get; set; } = "";
+
+        /// <summary>
+        /// 当前登录用户信息(未登录则为空)
+        /// </summary>
+        public int UserId { get; set; } = 0;
+
+        /// <summary>
+        /// 问题是否已解决
+        /// </summary>
+        public bool IsSolved { get; set; } = false;
+
+        /// <summary>
+        /// 问题解决方法
+        /// </summary>
+        public string Solution { get; set; } = "";
+
+        /// <summary>
+        /// 问题解决人
+        /// </summary>
+        public string SolvedBy { get; set; } = "";
+
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        public DateTime CreateDate { get; set; } = DateTime.Now;
+
+        public string StackTrace { get; set; } = "";
+    }
+
+    /// <summary>
+    /// 错误来源
+    /// </summary>
+    public enum ErrorFrom
+    {
+        /// <summary>
+        /// 网站
+        /// </summary>
+        Site = 0,
+    }
+}
---
+++ dotNetCoreApi/Api.Service/ErrorService.cs
@@ -0,0 +1,42 @@
+﻿using Api.Entity.Logs;
+using Api.Repository;
+using System;
+using System.Collections.Generic;
+
+namespace Api.Service
+{
+    public class ErrorService
+    {
+        /// <summary>
+        /// 保存一条错误信息
+        /// </summary>
+        /// <param name="error"></param>
+        public void AddError(ErrorEntity error)
+        {
+            try
+            {
+                BaseRepository<ErrorEntity>.Instance.Insert(error);
+            }
+            catch
+            {
+                // ignored
+            }
+        }
+
+        /// <summary>
+        /// 保存多条错误信息
+        /// </summary>
+        /// <param name="errors"></param>
+        public void AddError(IEnumerable<ErrorEntity> errors)
+        {
+            try
+            {
+                BaseRepository<ErrorEntity>.Instance.Insert(errors);
+            }
+            catch (Exception)
+            {
+                // ignored
+            }
+        }
+    }
+}
