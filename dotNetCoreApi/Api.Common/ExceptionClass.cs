a0ead58557e5b29038f7e0f5d08323c255c02dd3
吴正迪 <2274367387@qq.com>
2020-08-02 13:41:38 +0800

    增加对异常信息就行封装

---
+++ dotNetCoreApi/Api.Common/ExceptionClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Api.Common
+{
+    /// <summary>
+    /// 异常帮助类
+    /// </summary>
+    public class ExceptionClass
+    {
+        /// <summary>
+        /// 抛出异常方法
+        /// </summary>
+        public static Exception Throw(Exception ex)
+        {
+            return new Exception(ex.Message, ex);
+        }
+    }
+}
